413ea228451d5f7800b3debe75243a7538899109
balzsfs <balazs0147@gmail.com>
2024-04-14 11:21:55 +0200

    move components out from SingleSalary to App jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import { useState } from "react"
 import SingleSalaryCalculator from "./components/SingleSalaryCalculator"
 import {TotalSalaryContext} from "./contexts/TotalSalaryContext"
+import Tabs from "./components/Tabs"
 
 
 function App() {
@@ -21,11 +22,41 @@ function App() {
     'numberOfChildren' : 0,
     'numberOfDiscountedChildren' : 0
   }]);
+  const [currentIndex, setCurrentIndex] = useState(0);
+
+  const createNewTab = () =>{
+    const basicData = {
+      'name' : '',
+      'salary' : 0,
+      'discount' : 0,
+      'rangeInput':50,
+      'savedInput' : 0,
+      'plusFromMarriage' : 0,
+      'plusFromChildren' : 0,
+      'underDiscountSwitch' : false,
+      'showNewMarriage' : false,
+      'personalDiscountSwitch' : false,
+      'showDiscountFromChildren' : false,
+      'eligibleForNewMarriage' : false,
+      'eligibleForNewMarriageHidden' : true,
+      'numberOfChildren' : 0,
+      'numberOfDiscountedChildren' : 0
+    }
+    setTotalSalary(prevTotalSalary => [...prevTotalSalary, basicData]);
+  }
 
   return (
     <>
       <TotalSalaryContext.Provider value={[totalSalary,setTotalSalary]}>
-        <SingleSalaryCalculator/>
+        <div>
+        <Tabs createNewTab={createNewTab} setCurrentIndex={setCurrentIndex} currentIndex={currentIndex} totalSalary={totalSalary}/>
+          <div className="grid grid-cols-2 gap-5 px-5">
+            <SingleSalaryCalculator currentIndex={currentIndex}/>
+            <div className="container bg-slate-200 rounded-xl">
+
+            </div>
+        </div>
+    </div>
       </TotalSalaryContext.Provider>
     </>
   )
--- src/components/SingleSalaryCalculator.jsx
+++ src/components/SingleSalaryCalculator.jsx
@@ -1,56 +1,16 @@
 import PageName from "./PageName"
 import Salary from "./Salary"
 import Discounts from "./Discounts"
-import { useContext, useState } from "react"
 import SalaryOut from "./SalaryOut"
-import Tabs from "./Tabs"
-import { TotalSalaryContext } from "../contexts/TotalSalaryContext"
-
-const SingleSalaryCalculator = () => {
-  const [salary,setSalary] = useState(0);
-  const [discount,setDiscount] = useState(0);
-  const [plusFromMarriage, setPlusFromMarriage] = useState(0);
-  const [plusFromChildren, setPlusFromChildren] = useState(0);
-  const [currentIndex,setCurrentIndex] = useState(0);
-
-  const [totalSalary,setTotalSalary] = useContext(TotalSalaryContext);
-
-  const createNewTab = () =>{
-    const basicData = {
-      'name' : '',
-      'salary' : 0,
-      'discount' : 0,
-      'rangeInput':50,
-      'savedInput' : 0,
-      'plusFromMarriage' : 0,
-      'plusFromChildren' : 0,
-      'underDiscountSwitch' : false,
-      'showNewMarriage' : false,
-      'personalDiscountSwitch' : false,
-      'showDiscountFromChildren' : false,
-      'eligibleForNewMarriage' : false,
-      'eligibleForNewMarriageHidden' : true,
-      'numberOfChildren' : 0,
-      'numberOfDiscountedChildren' : 0
-    }
-    setTotalSalary(prevTotalSalary => [...prevTotalSalary, basicData]);
-  }
 
 
+const SingleSalaryCalculator = ({currentIndex}) => {
   return (
-    <div>
-        <Tabs createNewTab={createNewTab} setCurrentIndex={setCurrentIndex} currentIndex={currentIndex} totalSalary={totalSalary}/>
-        <div className="grid grid-cols-2 gap-5 px-5">
-          <div className="container bg-slate-200 rounded-xl p-5">
-              <PageName currentIndex={currentIndex} />
-              <Salary currentIndex={currentIndex} />
-              <Discounts currentIndex={currentIndex}/>
-              <SalaryOut salary={salary} discount={discount} plusFromChildren={plusFromChildren} plusFromMarriage={plusFromMarriage} currentIndex={currentIndex}/>
-          </div>
-        <div className="container bg-slate-200 rounded-xl">
-
-        </div>
-        </div>
+    <div className="container bg-slate-200 rounded-xl p-5">
+      <PageName currentIndex={currentIndex} />
+      <Salary currentIndex={currentIndex} />
+      <Discounts currentIndex={currentIndex}/>
+      <SalaryOut currentIndex={currentIndex}/>
     </div>
   )
 }
